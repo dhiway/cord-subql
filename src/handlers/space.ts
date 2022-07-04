49b711607eed5aee17eec14f783057d9b257fd0b
Amar Tumballi <amar@dhiway.com>
2022-07-04 18:33:45 +0530

    add missing file space.ts
    Signed-off-by: Amar Tumballi <amar@dhiway.com>

---
+++ src/handlers/space.ts
@@ -0,0 +1,22 @@
+import { ensureAccount } from "./account"
+import { ensureCallExist } from "./call"
+import { Space } from "../types"
+import { DispatchedCallData, EventHandler } from "./types"
+
+export const handleSpaceCreate: EventHandler = async ({ event, rawEvent }) => {
+    const [space_hash, identifier, creator] = rawEvent.event.data as unknown as [any, any, any]; //fix the types properly later
+
+    await ensureAccount(creator.toString())
+
+    const space = new Space(identifier.toString());
+
+    space.hash = space_hash.toString()
+    space.timestamp = rawEvent.block.timestamp
+    space.creator = creator.toString()
+    space.archived = false
+
+    await space.save()
+}
+
+
+
