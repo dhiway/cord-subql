8712d5429df10bd1802c9ce9aafc582b1c347eb1
Amar Tumballi <amar@dhiway.com>
2022-07-04 18:33:13 +0530

    working with space
    Signed-off-by: Amar Tumballi <amar@dhiway.com>

--- src/handlers/event.ts
+++ src/handlers/event.ts
@@ -5,82 +5,22 @@ import { Event } from "../types/models";
 import { getKVData } from "./utils";
 import { ensureExtrinsic } from "./extrinsic";
 import { DispatchedEventData } from "./types";
-// import { updateCrossedKSM } from './summary'
+import { handleSpaceCreate } from './space'
 
 const dispatch = new Dispatcher<DispatchedEventData>();
 
-/*
 dispatch.batchRegist([
-  // currencies
-  // { key: 'currencies-BalanceUpdated', handler: updateBalanceByUpdate },
-  // { key: 'currencies-Deposited', handler: updateBalanceByDeposit },
-  // { key: 'currencies-Withdrawn', handler: updateBalanceByWithdrawn },
-  // { key: 'currencies-Transferred', handler: updateBalanceByTransferred },
-  // { key: 'currencies-Withdrawn', handler: updateCrossedKSM },
-  // { key: 'currencies-Transferred', handler: updateCrossedKSM },
 
-  // nft
-  { key: "nft-TransferredToken", handler: createNFTTransferHistory },
-  { key: "nft-BurnedToken", handler: createNFTBurnedHistory },
-  { key: "nft-BurnedTokenWithRemark", handler: createNFTBurnedWithRemarkHistory },
+  //space
+  { key: 'space-Create', handler: handleSpaceCreate },
 
-  // loan
-  { key: "loans-PositionUpdated", handler: createPositionUpdatedHistory },
-  { key: "loans-PositionUpdated", handler: updateLoanPosition },
-  { key: "loans-ConfiscateCollateralAndDebit", handler: createConfiscateCollateralAndDebitHistory },
-  { key: "loans-transferLoan", handler: createTransferLoanHistory },
+  //schema
+  //{ key: 'schema-Anchor', handler: handleSchemaAnchor },
 
-  // // all cdp params config update
-  { key: "cdpEngine-InterestRatePerSecUpdated", handler: handleInterestRatePerSecUpdated, },
-  { key: "cdpEngine-LiquidationRatioUpdated", handler: handleLiquidationRatioUpdated, },
-  { key: "cdpEngine-LiquidationPenaltyUpdated", handler: handleLiquidationPenaltyUpdated, },
-  { key: "cdpEngine-RequiredCollateralRatioUpdated", handler: handleRequiredCollateralRatioUpdated, },
-  { key: "cdpEngine-MaximumTotalDebitValueUpdated", handler: handleMaximumTotalDebitValueUpdated, },
-  { key: "cdpEngine-GlobalInterestRatePerSecUpdated", handler: handleGlobalInterestRatePerSecUpdated, },
-  { key: "cdpEngine-LiquidateUnsafeCDP", handler: createLiquidateUnsafeCDPHistory },
-  { key: "cdpEngine-LiquidateUnsafeCDP", handler: updateLoanPositionByLiquidate },
-  { key: "honzon-CloseLoanHasDebitByDex", handler: handleCloseLoanHasDebitByDex},
-
-  // // dex
-  { key: "dex-ProvisioningToEnabled", handler: createDexPool },
-  { key: "dex-AddLiquidity", handler: updatePoolByAddLiquidity },
-  { key: "dex-RemoveLiquidity", handler: updatePoolByRemoveLiquidity },
-  { key: "dex-Swap", handler: updatePoolBySwap },
-
-  // // provision
-  { key: "dex-ListProvision", handler: createProvision },
-  { key: "dex-ProvisioningToEnabled", handler: updateProvisionByEnable },
-  { key: "dex-AddProvision", handler: updateUserProvision },
-
-  // // incentive
-  { key: "incentives-DepositDexShare", handler: createDepositDexShareHistory },
-  { key: "incentives-WithdrawDexShare", handler: createWithdrawDexShareHistory },
-  { key: "incentives-PayoutRewards", handler: createClaimRewards },
-  { key: "incentives-ClaimRewards", handler: createClaimRewards },
-
-  // homa lite
-  { key: 'homaLite-Minted', handler: createHomaLiteMintHistory },
-  { key: 'homaLite-RedeemRequestCancelled', handler: createHomaLiteRedeemCancelHistory },
-  { key: 'homaLite-RedeemRequested', handler: createHomaLiteRedeemRequestHistory },
-  { key: 'homaLite-Redeemed', handler: createHomaLiteRedeemedHistory },
-
-  // homa
-  { key: 'homa-Minted', handler: handleHomaMinted },
-  { key: 'homa-RequestedRedeem', handler: handleHomaRequestedRedeem},
-  { key: 'homa-RequestedCancelled', handler: handleHomaRequestedCancelled},
-  { key: 'homa-RedeemedByFastMatch', handler: handleHomaRedeemedByFastMatch},
-  { key: 'homa-RedeemedByUnbond', handler: handleHomaRedeemedByUnbond },
-
-  // { key: "stakingPool-MintLiquid", handler: createMintLiquidHistory },
-  // { key: "stakingPool-RedeemByUnbond", handler: createRedeemByUnbondHistory },
-  // { key: "stakingPool-RedeemByFreeUnbonded", handler: createRedeemByFreeUnbonded },
-  // { key: "stakingPool-RedeemByClaimUnbonding", handler: createRedeemByClaimUnbonding },
-]);
-*/
-
-dispatch.batchRegist([
-  // stream
-  //{ key: 'strean-Anchored', handler: theFunctionToCall },
+  //stream
+  //{ key: 'stream-Anchor', handler: handleStreamAnchor },
+  //{ key: 'stream-Update', handler: handleStreamUpdate },
+  //{ key: 'stream-Revoke', handler: handleStreamRevoke },
 ]);
 
 export async function ensureEvent(event: SubstrateEvent) {
@@ -128,12 +68,11 @@ export async function createEvent(event: SubstrateEvent) {
   }
 
 // TODO: once we have separate handling of different events based on section and method, uncomment below
-/*
   await dispatch.dispatch(`${section}-${data.method}`, {
     event: data,
     rawEvent: event,
   });
-*/
+
   await data.save();
 
   return data;
