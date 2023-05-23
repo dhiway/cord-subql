e2740e24c62b2d1be5f5e6ec3e87f23b38c4b810
Amar Tumballi <amar@dhiway.com>
2023-05-23 13:03:37 +0530

    for now, make all the events as no-op to get started
    Signed-off-by: Amar Tumballi <amar@dhiway.com>

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -7,18 +7,25 @@ import { StarterEntity } from "../types";
 import { Balance } from "@polkadot/types/interfaces";
 import assert from "assert";
 
+const URL = require('url').URL
+
 export async function handleBlock(block: SubstrateBlock): Promise<void> {
   //Create a new starterEntity with ID using block hash
+  console.log("Test", block.block);
+  /*
   let record = new StarterEntity(
-      block.block.header.hash.toString(),
-      block.block.header.number.toNumber()
+      block.block.header.hash.toString()
   );
 
   //Record block number
   await record.save();
+  */
 }
 
 export async function handleEvent(event: SubstrateEvent): Promise<void> {
+  console.log("Test Event", event);
+
+/*
   const {
     event: {
       data: [account, balance],
@@ -30,21 +37,25 @@ export async function handleEvent(event: SubstrateEvent): Promise<void> {
   );
   assert(record, "record does not exist")
 
-  record.field2 = account.toString();
+  record.account = account.toString();
   //Big integer type Balance of a transfer event
-  record.field3 = (balance as Balance).toBigInt();
+  record.balance = (balance as Balance).toBigInt();
   await record.save();
+*/
 }
 
 export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
+  console.log("Test Call", extrinsic);
   const record = await StarterEntity.get(
     extrinsic.block.block.header.hash.toString()
   );
-  assert(record, "record does not exist")
+  /*
+   assert(record, "record does not exist")
 
   //Date type timestamp
-  record.field4 = extrinsic.block.timestamp;
+  record.date = extrinsic.block.timestamp;
   //Boolean type
-  record.field5 = true;
+  record.status = true;
   await record.save();
+  */
 }
--- src/test/mappingHandlers.test.ts
+++ src/test/mappingHandlers.test.ts
@@ -8,7 +8,6 @@ subqlTest(
     [
       StarterEntity.create({
         id: '0xe430ab806de0b5f4a07140b2f708a6f6ccd1a5fbd9c8e1ca98e936c48ae757b8', 
-        field1: 191,
       }),
     ],
     'handleBlock',
