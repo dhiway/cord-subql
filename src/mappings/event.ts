0ff8d86617444ad4a6b850ba1a4b4dcca63a7245
Amar Tumballi <amar@dhiway.com>
2023-12-06 22:08:59 +0530

    few calls moved
    Signed-off-by: Amar Tumballi <amar@dhiway.com>

---
+++ src/mappings/event.ts
@@ -0,0 +1,78 @@
+import { SubstrateEvent } from "@subql/types";
+import { Dispatcher } from "./utils/dispatcher";
+import { ensureBlock } from "./block";
+import { Event } from "../types/models";
+import { getKVData } from "./utils";
+import { ensureExtrinsic } from "./extrinsic";
+import { DispatchedEventData } from "./types";
+
+const dispatch = new Dispatcher<DispatchedEventData>();
+
+dispatch.batchRegist([
+
+  //registry
+  //{ key: 'schema-Create', handler: handleRegistryCreate },
+
+  //schema
+  //{ key: 'schema-Anchor', handler: handleSchemaAnchor },
+
+  //stream
+  //{ key: 'stream-Anchor', handler: handleStreamAnchor },
+  //{ key: 'stream-Update', handler: handleStreamUpdate },
+  //{ key: 'stream-Revoke', handler: handleStreamRevoke },
+]);
+
+export async function ensureEvent(event: SubstrateEvent) {
+  const block = await ensureBlock(event.block);
+  if (!block) {
+      return null;
+  }
+  const idx = event.idx;
+  const recordId = `${block.number}-${idx}`;
+
+  let data = await Event.get(recordId);
+
+  if (!data) {
+    data = new Event(recordId);
+    data.index = idx;
+    data.blockId = block.id;
+    data.blockNumber = block.number;
+    data.timestamp = block.timestamp;
+  }
+
+  return data;
+}
+
+export async function createEvent(event: SubstrateEvent) {
+  const data = await ensureEvent(event);
+  if (!data)
+      return;
+  const section = event.event.section;
+  const method = event.event.method;
+  const eventData = getKVData(event.event.data);
+
+  if (section === 'system' && method === 'ExtrinsicSuccess')
+     return;
+
+  data.section = section;
+  data.method = method;
+  data.data = eventData;
+
+  const extrinsic = await (event.extrinsic
+    ? ensureExtrinsic(event.extrinsic)
+    : undefined);
+
+  if (extrinsic) {
+    data.extrinsicId = extrinsic.id;
+  }
+
+// TODO: once we have separate handling of different events based on section and method, uncomment below
+  await dispatch.dispatch(`${section}-${data.method}`, {
+    event: data,
+    rawEvent: event,
+  });
+
+  await data.save();
+
+  return data;
+}
--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -7,61 +7,22 @@ import { Account, Transfer } from "../types";
 import { Balance } from "@polkadot/types/interfaces";
 import { decodeAddress } from "@polkadot/util-crypto";
 
+import { createExtrinsic } from './extrinsic';
+import { createEvent } from './event';
+
 export async function handleBlock(block: SubstrateBlock): Promise<void> {
   // Do something with each block handler here
+  /* most of the blocks would be blank with timestamp only, so ignore for now */
 }
 
 export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
   // Do something with a call handler here
+  /* Store every extrinsic other than timestamp */
+  createExtrinsic(extrinsic);
 }
 
 export async function handleEvent(event: SubstrateEvent): Promise<void> {
-  logger.info(
-    `New transfer event found at block ${event.block.block.header.number.toString()}`
-  );
-
-  // Get data from the event
-  // The balances.transfer event has the following payload \[from, to, value\]
-  // logger.info(JSON.stringify(event));
-  const {
-    event: {
-      data: [from, to, amount],
-    },
-  } = event;
-
-  const blockNumber: number = event.block.block.header.number.toNumber();
-
-  const fromAccount = await checkAndGetAccount(from.toString(), blockNumber);
-  const toAccount = await checkAndGetAccount(to.toString(), blockNumber);
-
-  // Create the new transfer entity
-  const transfer = Transfer.create({
-    id: `${event.block.block.header.number.toNumber()}-${event.idx}`,
-    blockNumber,
-    date: event.block.timestamp,
-    fromId: fromAccount.id,
-    toId: toAccount.id,
-    amount: (amount as Balance).toBigInt(),
-  });
-
-  fromAccount.lastTransferBlock = blockNumber;
-  toAccount.lastTransferBlock = blockNumber;
-
-  await Promise.all([fromAccount.save(), toAccount.save(), transfer.save()]);
+  createEvent(event);
 }
 
-async function checkAndGetAccount(
-  id: string,
-  blockNumber: number
-): Promise<Account> {
-  let account = await Account.get(id.toLowerCase());
-  if (!account) {
-    // We couldn't find the account
-    account = Account.create({
-      id: id.toLowerCase(),
-      publicKey: decodeAddress(id).toString().toLowerCase(),
-      firstTransferBlock: blockNumber,
-    });
-  }
-  return account;
-}
+
