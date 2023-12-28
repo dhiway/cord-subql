bf43ac3367880eebb90471b3731f1fe307bc1f16
Ujjwal Sharma <ujjwal.sharma@dhiway.com>
2023-12-28 11:27:44 +0530

    [fix] build issues, added mixing dependencies.

--- src/handlers/utils/dispatcher.ts
+++ src/handlers/utils/dispatcher.ts
@@ -1,29 +1,34 @@
-export type DispatcherHandler<T> = (data?: T) => Promise<void>
+export type DispatcherHandler<T> = (data?: T) => Promise<void>;
 
 export class Dispatcher<DispatchData> {
-  private subHandlers: Record<string, DispatcherHandler<DispatchData>[]> = {}
+  private subHandlers: Record<string, DispatcherHandler<DispatchData>[]> = {};
 
-  public regist (key: string, handler: DispatcherHandler<DispatchData>) {
-    if (Reflect.has(this.subHandlers, key) && typeof Array.isArray(this.subHandlers[key])) {
-      this.subHandlers[key].push(handler)
+  public regist(key: string, handler: DispatcherHandler<DispatchData>) {
+    if (
+      Reflect.has(this.subHandlers, key) &&
+      typeof Array.isArray(this.subHandlers[key])
+    ) {
+      this.subHandlers[key].push(handler);
     } else {
-      this.subHandlers[key] = [handler]
+      this.subHandlers[key] = [handler];
     }
   }
 
-  public batchRegist (list: { key: string; handler: DispatcherHandler<DispatchData> }[]) {
-    list.forEach((item) => this.regist(item.key, item.handler))
+  public batchRegist(
+    list: { key: string; handler: DispatcherHandler<DispatchData> }[]
+  ) {
+    list.forEach((item) => this.regist(item.key, item.handler));
   }
 
-  public async dispatch (key: string, data: DispatchData) {
-    const handlers = this.subHandlers[key]
+  public async dispatch(key: string, data: DispatchData) {
+    const handlers = this.subHandlers[key];
 
-    if (!handlers) return
+    if (!handlers) return;
 
     await Promise.all(handlers.map((handler) => handler(data))).catch((e) => {
-      logger.info(e.stack.toString())
+      // logger.info(e.stack.toString())
 
-      throw e
-    })
+      throw e;
+    });
   }
 }
