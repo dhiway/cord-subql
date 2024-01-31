1957f7e57009abac9cb5e444db6d2d330b7c6ec8
Ujjwal Sharma <ujjwal.sharma@dhiway.com>
2024-01-31 15:54:55 +0530

    fix: chainspace undefined enteries ignored

--- src/handlers/chainSpace.ts
+++ src/handlers/chainSpace.ts
@@ -9,34 +9,29 @@ export async function createChainSpace(
 ): Promise<void> {
   const data = extrinsic.extrinsic.method;
 
-  // logger.info("\n\n ChainSpace");
-  // logger.info("\n Data Event:");
-  // logger.info(extrinsic.events.toString());
-  // logger.info("\n\n ChainSpace ENDED");
-
   let arrayed = JSON.parse(JSON.stringify(data));
-  // logger.info(
-  //   `\n\n\n\n ChainSpace Method: ${method} \n ${JSON.stringify(arrayed)}`
-  // );
   let chainSpaceId = JSON.parse(JSON.stringify(extrinsic.events))[0].event
     .data[0];
   let chainspace = new ChainSpace(id);
 
-  if (method === "create") {
-    chainspace.method = method;
+  chainspace.method = method;
+  if (arrayed.args.did_call.blockNumber) {
     chainspace.blockNumber = arrayed.args.did_call.blockNumber;
-    chainspace.submitter = arrayed.args.did_call.submitter;
+  }
+  if (arrayed.args.signature.ed25519) {
     chainspace.signature = arrayed.args.signature.ed25519;
+  }
+  if (chainSpaceId) {
     chainspace.chainspace_id = chainSpaceId;
-    chainspace.space_id = arrayed.args.did_call.call.args.space_code;
-  } else {
-    chainspace.method = method;
-    chainspace.blockNumber = arrayed.args.did_call.blockNumber;
-    chainspace.submitter = arrayed.args.did_call.submitter;
-    chainspace.signature = arrayed.args.signature.ed25519;
-    chainspace.chainspace_id = chainSpaceId;
+  }
+  if (arrayed.args.did_call.call.args.authorization) {
     chainspace.authorization = arrayed.args.did_call.call.args.authorization;
-    chainspace.space_id = arrayed.args.did_call.call.args.space_id;
+  }
+  if (arrayed.args.did_call.call.args.space_id || chainSpaceId) {
+    chainspace.space_id =
+      arrayed.args.did_call.call.args.space_id || chainSpaceId;
+  }
+  if (arrayed.args.did_call.call.args.delegate) {
     chainspace.delegate = arrayed.args.did_call.call.args.delegate;
   }
 
