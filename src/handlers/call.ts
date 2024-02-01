cd5230c3281f254a2f84b1fe4bd961642ad28b73
Ujjwal Sharma <ujjwal.sharma@dhiway.com>
2024-02-02 01:07:05 +0530

    feat: added schema call indexing

--- src/handlers/call.ts
+++ src/handlers/call.ts
@@ -1,14 +1,18 @@
-import type { Vec } from '@polkadot/types'
-import { Call, Extrinsic } from '../types'
-import { AnyCall, DispatchedCallData } from './types'
+import type { Vec } from "@polkadot/types";
+import { Call, Extrinsic } from "../types";
+import { AnyCall, DispatchedCallData } from "./types";
 
 import { SubstrateExtrinsic } from "@subql/types";
-import { Dispatcher, getBatchInterruptedIndex, getKVData } from './utils';
-import { createScore } from './score';
+import { Dispatcher, getBatchInterruptedIndex, getKVData } from "./utils";
+import { createScore } from "./score";
+import { createSchema } from "./schema";
 
-async function traverExtrinsic(extrinsic: Extrinsic, raw: SubstrateExtrinsic): Promise<Call[]> {
-  const list: any[] = []
-  const batchInterruptedIndex = getBatchInterruptedIndex(raw)
+async function traverExtrinsic(
+  extrinsic: Extrinsic,
+  raw: SubstrateExtrinsic
+): Promise<Call[]> {
+  const list: any[] = [];
+  const batchInterruptedIndex = getBatchInterruptedIndex(raw);
 
   const inner = async (
     data: AnyCall,
@@ -17,35 +21,35 @@ async function traverExtrinsic(extrinsic: Extrinsic, raw: SubstrateExtrinsic): P
     isRoot: boolean,
     depth: number
   ) => {
-    const id = isRoot ? parentCallId : `${parentCallId}-${idx}`
-    const method = data.method
-    const section = data.section
-    const args = data.args
+    const id = isRoot ? parentCallId : `${parentCallId}-${idx}`;
+    const method = data.method;
+    const section = data.section;
+    const args = data.args;
 
-    if (method === 'submitDidCall' && section === 'did') {
-	/* there will be one call below */
-	 const temp = (args[0] as any).call as unknown as AnyCall;
-	 logger.info(`DidSubmitCall:  ${temp}`);
-	 await inner(temp, id, 1, false, depth + 1);
+    if (method === "submitDidCall" && section === "did") {
+      /* there will be one call below */
+      const temp = (args[0] as any).call as unknown as AnyCall;
+      await inner(temp, id, 1, false, depth + 1);
     }
-    const call = new Call(id)
+    const call = new Call(id);
 
-    call.method = method
-    call.section = section
-    call.args = getKVData(data.args, data.argsDef)
-    call.signerId = extrinsic.signerId
-    call.isSuccess = depth === 0 ? extrinsic.isSuccess : batchInterruptedIndex > idx;
-    call.timestamp = extrinsic.timestamp
+    call.method = method;
+    call.section = section;
+    call.args = getKVData(data.args, data.argsDef);
+    call.signerId = extrinsic.signerId;
+    call.isSuccess =
+      depth === 0 ? extrinsic.isSuccess : batchInterruptedIndex > idx;
+    call.timestamp = extrinsic.timestamp;
 
     if (!isRoot) {
-      call.parentCallId = isRoot ? '' : parentCallId
+      call.parentCallId = isRoot ? "" : parentCallId;
 
-      call.extrinsicId = parentCallId.split('-')[0]
+      call.extrinsicId = parentCallId.split("-")[0];
     } else {
-      call.extrinsicId = parentCallId
+      call.extrinsicId = parentCallId;
     }
 
-    list.push(call)
+    list.push(call);
 
     /*
     await dispatcher.dispatch(
@@ -53,38 +57,50 @@ async function traverExtrinsic(extrinsic: Extrinsic, raw: SubstrateExtrinsic): P
       { call, extrinsic, rawCall: data, rawExtrinsic: raw }
     )
     */
-    if (call.section === 'networkScore') {
-       logger.info("Scoring call");
-       await createScore(raw, id as string);
+    if (call.section === "networkScore") {
+      logger.info("Scoring call");
+      await createScore(raw, id as string);
     }
-       
-    if (depth < 1 && section === 'utility' && (method === 'batch' || method === 'batchAll')) {
-      const temp = args[0] as unknown as Vec<AnyCall>
 
-      await Promise.all(temp.map((item, idx) => inner(item, id, idx, false, depth + 1)))
-    } 
-  }
+    if (section === "schema") {
+      logger.info("Schema call");
+      await createSchema(raw, call, id as string, data.method);
+    }
 
-  await inner(raw.extrinsic.method, extrinsic.id, 0, true, 0)
+    if (
+      depth < 1 &&
+      section === "utility" &&
+      (method === "batch" || method === "batchAll")
+    ) {
+      const temp = args[0] as unknown as Vec<AnyCall>;
 
-  return list
+      await Promise.all(
+        temp.map((item, idx) => inner(item, id, idx, false, depth + 1))
+      );
+    }
+  };
+
+  await inner(raw.extrinsic.method, extrinsic.id, 0, true, 0);
+
+  return list;
 }
 
-export async function createCalls (extrinsic: Extrinsic, raw: SubstrateExtrinsic) {
-
-    const calls = await traverExtrinsic(extrinsic, raw)
-    logger.info(`Calls: ${calls}`)
-    await Promise.all(calls.map(async (item) => item.save()));
+export async function createCalls(
+  extrinsic: Extrinsic,
+  raw: SubstrateExtrinsic
+) {
+  const calls = await traverExtrinsic(extrinsic, raw);
+  await Promise.all(calls.map(async (item) => item.save()));
 }
 
-export async function ensureCallExist (id: any) {
-  let data = await Call.get(id)
+export async function ensureCallExist(id: any) {
+  let data = await Call.get(id);
 
   if (!data) {
-    data = new Call(id)
+    data = new Call(id);
 
-    await data.save()
+    await data.save();
   }
 
-  return data
+  return data;
 }
