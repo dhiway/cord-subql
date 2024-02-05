a59cbe5c23e599c20c48fccb2a543dfddcdfd64d
Ujjwal Sharma <ujjwal.sharma@dhiway.com>
2024-02-05 17:49:06 +0530

    fix: code merge issues

--- src/handlers/call.ts
+++ src/handlers/call.ts
@@ -5,13 +5,34 @@ import { AnyCall, DispatchedCallData } from "./types";
 import { SubstrateExtrinsic } from "@subql/types";
 import { Dispatcher, getBatchInterruptedIndex, getKVData } from "./utils";
 import { createScore } from "./score";
-import { indexDidCall } from './did'
+import { indexDidCall } from "./did";
 import { createStatement } from "./statement";
 import { indexAssetCall } from "./asset";
 import { indexIdentityCall } from "./identity";
 import { handleDidNameCall } from "./didName";
 import { createSchema } from "./schema";
 import { createChainSpace } from "./chainSpace";
+
+export async function createCalls(
+  extrinsic: Extrinsic,
+  raw: SubstrateExtrinsic
+) {
+  const calls = await traverExtrinsic(extrinsic, raw);
+  logger.info(`Calls: ${calls}`);
+  await Promise.all(calls.map(async (item) => item.save()));
+}
+
+export async function ensureCallExist(id: any) {
+  let data = await Call.get(id);
+
+  if (!data) {
+    data = new Call(id);
+
+    await data.save();
+  }
+
+  return data;
+}
 
 async function traverExtrinsic(
   extrinsic: Extrinsic,
@@ -35,7 +56,7 @@ async function traverExtrinsic(
     if (method === "submitDidCall" && section === "did") {
       /* there will be one call below */
       const temp = (args[0] as any).call as unknown as AnyCall;
-      logger.info(`DidSubmitCall:  ${temp}`);
+      // logger.info(`DidSubmitCall:  ${temp}`);
       await inner(temp, id, 1, false, depth + 1);
     }
     const call = new Call(id);
@@ -64,47 +85,46 @@ async function traverExtrinsic(
       { call, extrinsic, rawCall: data, rawExtrinsic: raw }
     )
     */
-    
-    if (call.section === 'networkScore') {
-       logger.info("Scoring call");
-       await createScore(raw, id as string, data.method);
-      
+
+    if (call.section === "networkScore") {
+      logger.info("Scoring call");
+      await createScore(raw, id as string, data.method);
+    }
+
     if (call.section === "statement") {
       logger.info(`${data.method}`);
       await createStatement(raw, id as string, data.method);
     }
 
-    if (call.section === 'did') {
-      logger.info("DID call")
-      await indexDidCall(raw, id as string, data.method)
+    if (call.section === "did") {
+      logger.info("DID call");
+      await indexDidCall(raw, id as string, data.method);
     }
-    
+
     if (call.section === "asset") {
       logger.info(`${data.method}`);
       await indexAssetCall(raw, id as string, data.method);
     }
-      
+
     if (section === "schema") {
       logger.info("Schema call");
       await createSchema(raw, call, id as string, data.method);
     }
-    
+
     if (call.section === "chainSpace") {
       logger.info("ChainSpace call");
       await createChainSpace(raw, call, id as string, data.method);
     }
-    
+
     if (call.section === "identity") {
       logger.info(`${data.method}`);
       await indexIdentityCall(raw, id as string, data.method);
     }
-      
+
     if (call.section === "didName") {
       logger.info(`${data.method}`);
       await handleDidNameCall(raw, id as string, data.method);
     }
-    if (depth < 1 && section === 'utility' && (method === 'batch' || method === 'batchAll')) {
-      const temp = args[0] as unknown as Vec<AnyCall>
 
     if (
       depth < 1 &&
@@ -122,25 +142,4 @@ async function traverExtrinsic(
   await inner(raw.extrinsic.method, extrinsic.id, 0, true, 0);
 
   return list;
-}
-
-export async function createCalls(
-  extrinsic: Extrinsic,
-  raw: SubstrateExtrinsic
-) {
-  const calls = await traverExtrinsic(extrinsic, raw);
-  logger.info(`Calls: ${calls}`);
-  await Promise.all(calls.map(async (item) => item.save()));
-}
-
-export async function ensureCallExist(id: any) {
-  let data = await Call.get(id);
-
-  if (!data) {
-    data = new Call(id);
-
-    await data.save();
-  }
-
-  return data;
 }
---
+++ src/handlers/networkMembership.ts
@@ -0,0 +1,52 @@
+import { SubstrateExtrinsic } from "@subql/types";
+import { ChainSpace, Call } from "../types";
+
+export async function createNetworkMembership(
+  extrinsic: SubstrateExtrinsic,
+  call: Call,
+  id: string,
+  method: string
+): Promise<void> {
+  const data = extrinsic.extrinsic.method;
+
+  let arrayed = JSON.parse(JSON.stringify(data));
+  // let chainSpaceId = JSON.parse(JSON.stringify(extrinsic.events))[0].event
+  //   .data[0];
+
+  logger.info(
+    `\n\n Network data \n Arrayed: \n ${JSON.stringify(
+      arrayed.args
+    )}\n extrinsic \n ${JSON.stringify(
+      extrinsic.block.events
+    )} \n\n ${JSON.stringify(extrinsic.events)}`
+  );
+  // let networkMembership = new ChainSpace(id);
+  //
+  // networkMembership.method = method;
+  // networkMembership.callIndex = arrayed.args.did_call.call.callIndex;
+  // networkMembership.chainspace_id = chainSpaceId;
+  // networkMembership.blockNumber =
+  //   extrinsic.block.block.header.number.toBigInt();
+  // networkMembership.signature = JSON.stringify(arrayed.args.signature);
+  //
+  // if (method === "create") {
+  //   networkMembership.space_code = arrayed.args.did_call.call.args.space_code;
+  // }
+  //
+  // if (method === "addDelegate") {
+  //   networkMembership.space_id = arrayed.args.did_call.call.args.space_id;
+  //   networkMembership.delegate = arrayed.args.did_call.call.args.delegate;
+  //   networkMembership.authorization =
+  //     arrayed.args.did_call.call.args.authorization;
+  //   networkMembership.submitter = arrayed.args.did_call.submitter;
+  // }
+  //
+  // if (method === "approve") {
+  //   // ToDo:
+  //   // Develop a demo script within the GitHub repository at github.com/dhiway/cord.js which
+  //   // calls this method. Execute the script to capture the extrinsic information, analyze the obtained data,
+  //   // and complete the implementation.
+  // }
+
+  // await networkMembership.save();
+}
